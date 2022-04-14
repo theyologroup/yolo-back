6bc01991031fd2c8147794765d594d2404e98775
Wenkang Zhou - vn52imw <Wenkang.Zhou@samsclub.com>
2022-04-13 21:15:46 -0700

    Initial boot

---
+++ src/main/java/com/theyologroup/yoloback/YoloBackApplication.java
@@ -0,0 +1,13 @@
+package com.theyologroup.yoloback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoloBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoloBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theyologroup/yoloback/YoloBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theyologroup.yoloback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoloBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
